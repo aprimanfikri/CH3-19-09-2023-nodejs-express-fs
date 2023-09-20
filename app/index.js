9dede89a58971343fca3389ec4dc4febc1de1153
aprimanfikri <aprmnfkr@gmail.com>
2023-09-20 19:39:15 +0700

    Refactor project structure: Remove and reorganize folders.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const tour = require("./tour");
+const tour = require("../routes/tour");
 
 app.use(morgan("dev"));
 app.use(express.json());
