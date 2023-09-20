0e4ca8da0e9aabf0d4e6191e76f50cc33a6df264
aprimanfikri <aprmnfkr@gmail.com>
2023-09-20 22:45:48 +0700

    Removing uppercase characters from the character set used in generating random IDs.

--- utils/random.js
+++ utils/random.js
@@ -1,5 +1,5 @@
 function generateId() {
-    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyz';
     let id = '';
     for (let i = 0; i < 24; i++) {
         const randomIndex = Math.floor(Math.random() * chars.length);
